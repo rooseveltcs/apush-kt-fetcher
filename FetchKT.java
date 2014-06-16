87a0122f9bac36647457e754331f8988b134a21f
Simone Archer-Krauss <sushisnak@gmail.com>
2014-06-16 12:26:57 -0700

    2nd version of FetchKT.java, comments were added

--- FetchKT.java
+++ FetchKT.java
@@ -11,79 +11,80 @@ import java.io.*;
 import java.awt.*;
  
 public class FetchKT {
-   public static int KTAnswer = 1;
+   public static int KTAnswer = 1; //class constant that keeps track of the number of keyterms that user has asked for
    public static void main(String[] args)throws FileNotFoundException {
-      File joking = new File("jokes.txt");
+      File joking = new File("jokes.txt"); //imports jokes file
       // Program asks for chapter, saves name of file, creates File object, creates Scanner object of File
       System.out.println("What chapter do you want to access? \n **Write chapter and the chapter number and add .txt, NO SPACES. Ex: chapter1.txt");
       Scanner input = new Scanner(System.in); //Scanner that asks for the chapter that the user wants
       String chapterFileName = input.next(); //holds the chapter and becomes a txt file
       System.out.println();
-      File chapterFile = new File (chapterFileName); //new
-      Scanner keyTerms = new Scanner(chapterFile);      
+      File chapterFile = new File (chapterFileName); //imports the file that the user writes in (chapterFileName)
+      Scanner keyTerms = new Scanner(chapterFile); //allows keyTerms to search through chapterFile      
     
       // Program asks for keyterm to search for, saves it
-      System.out.print("What keyterm would you like? ");
-      String kT = input.next();
-      kT += input.nextLine();
-      System.out.println();
+      System.out.print("What keyterm would you like? "); //asks the user for the keyterm that they want
+      String kT = input.next(); //asks the user for the keyterm that they want
+      kT += input.nextLine(); //looks at the first word in the user input line
+      System.out.println(); //looks at the rest of the user input line
        
-      name(input, chapterFileName, chapterFile, keyTerms, kT, joking);
+      name(input, chapterFileName, chapterFile, keyTerms, kT, joking); //calsl name method
    
    }
+   //askUser method is called by name and joke methods if the user wants to continue looking for other keyterms
    public static void askUser(Scanner input, String chapterFileName, File chapterFile, Scanner keyTerms, String kT, File joking)throws FileNotFoundException{
       System.out.println();
-         // Program asks for chapter, saves name of file, creates File object, creates Scanner object of File
+      // Program asks for chapter, saves name of file, creates File object, creates Scanner object of File
       System.out.println("What chapter do you want to access? \n **Write chapter and the chapter number and add .txt, NO SPACES. Ex: chapter1.txt");
       input = new Scanner(System.in); //Scanner that asks for the chapter that the user wants
       chapterFileName = input.next(); //holds the chapter and becomes a txt file
       System.out.println();
-      chapterFile = new File (chapterFileName); //new
-      keyTerms = new Scanner(chapterFile);      
+      chapterFile = new File (chapterFileName); //imports the file that the user writes in (chapterFileName)
+      keyTerms = new Scanner(chapterFile); //allows keyTerms to search through chapterFile
       
       // Program asks for keyterm to search for, saves it
-      System.out.print("What keyterm would you like? ");
-      kT = input.next();
-      kT += input.nextLine();
-      System.out.println();
+      System.out.print("What keyterm would you like? "); //asks the user for the keyterm that they want
+      kT = input.next(); //asks the user for the keyterm that they want
+      kT += input.nextLine(); //looks at the first word in the user input line
+      System.out.println(); //looks at the rest of the user input line
       
-      name(input, chapterFileName, chapterFile, keyTerms, kT, joking);
+      name(input, chapterFileName, chapterFile, keyTerms, kT, joking); //calls name method
    }
    //prints out the name
    //checks the name/keyterm - the first line of each set
    public static void name (Scanner input, String chapterFileName, File chapterFile, Scanner keyTerms, String kT, File joking) throws FileNotFoundException{
-      boolean hasKT = false;
-      String currentKT = "";
-      String def = "";
-      String sig = "";
+      boolean hasKT = false; //boolean checking to see if the search has found the keyterm
+      String currentKT = ""; //keeps track of the current keyterm
+      String def = ""; //keeps track of the current definition
+      String sig = ""; //keeps track of the current sigificance
       
-      while(keyTerms.hasNextLine() && hasKT == false){
-         currentKT = keyTerms.nextLine();
-         def = keyTerms.nextLine();
-         sig = keyTerms.nextLine();
-         hasKT = kT.equalsIgnoreCase(currentKT); 
+      while(keyTerms.hasNextLine() && hasKT == false){ 
+         currentKT = keyTerms.nextLine(); //runs through KT line
+         def = keyTerms.nextLine(); //runs through def line
+         sig = keyTerms.nextLine(); //runs through sig line
+         hasKT = kT.equalsIgnoreCase(currentKT); //checks to see if the user input keyterm is equal to the current keyterm the program has
       
       }
       
       if(hasKT){ //checks if the KT is in the txt file
          System.out.println(currentKT + ": ");
          System.out.println();
-         definitions(def);
-         significance(sig);
-         joke(input, chapterFileName, chapterFile, keyTerms, kT, joking);
+         definitions(def); //calls definitions method
+         significance(sig); //calls siginificance method
+         joke(input, chapterFileName, chapterFile, keyTerms, kT, joking); //calls joke method
       }
       else {
          System.out.println("Sorry. The keyterm you asked for is not in this chapter. Try a different chapter or check your spelling.");
          System.out.println();
          System.out.print("Would you like to try again? ");
-         String again = input.next();
+         String again = input.next(); //asks the user if they want to try again in the case of a wrong keyterm
          System.out.println();
-         if(again.startsWith("y")){
-            joke(input, chapterFileName, chapterFile, keyTerms, kT, joking);
+         if(again.startsWith("y")){ //if the user says yes
+            joke(input, chapterFileName, chapterFile, keyTerms, kT, joking); //calls joke method
          }
          else {
             System.out.println("Thank you.");
-            System.exit(0);
+            System.exit(0); //exits the entire program
          }
       }
    }  
@@ -100,36 +101,34 @@ public class FetchKT {
       System.out.println(sig);
       System.out.println();
    }
-   //joke method
+   //joke method, asks if the user has another keyterm, adds to KTAnswer, checks how many keyterms have been asked for and if there are 10 then it prints out a joke, asks for the answer to the joke, calls askUser method
    public static void joke(Scanner input, String chapterFileName, File chapterFile, Scanner keyTerms, String kT, File joking) throws FileNotFoundException{
       Scanner jokes = new Scanner(joking);
       System.out.print("Do you have another keyterm? ");
-      String answer = input.next();
+      String answer = input.next(); //asks the user if they would like to search for another keyterm
       System.out.println();
-      //int kTNumber = 1;
       String jk = ""; 
       String punchLine = "";
       while(answer.startsWith("y")){
-         KTAnswer++;
+         KTAnswer++; //adds to KTAnswer
          if(KTAnswer % 10 == 0){
-            jk = jokes.nextLine();
-            punchLine = jokes.nextLine();
-            System.out.println(jk);
-            String userAnswer = input.next();
-            userAnswer += input.nextLine();
-            if(userAnswer.equalsIgnoreCase(punchLine)){
+            jk = jokes.nextLine(); //gets joke
+            punchLine = jokes.nextLine(); //gets punchLine
+            System.out.println(jk); //prints out joke
+            String userAnswer = input.next(); //asks for the user's answer
+            userAnswer += input.nextLine(); //adds the rest of the user's answer line
+            if(userAnswer.equalsIgnoreCase(punchLine)){ //if they type in the answer and it is correct
                System.out.println("Congratulations! You guessed correctly!");
             }
-            else {
-               System.out.println("Too bad. The correct answer is: " + punchLine);
+            else { //if the answer the user types in is incorrect
+               System.out.println("Too bad. The correct answer is: " + punchLine); //prints out punchLine
             }
          }
-         askUser(input, chapterFileName, chapterFile, keyTerms, kT, joking);
+         askUser(input, chapterFileName, chapterFile, keyTerms, kT, joking); //calls askUser method
       }
-      //answer.substring(0, 1).equalsIgnoreCase("n") {
-      if(answer.startsWith("n")){
+      if(answer.startsWith("n")){ //if the user does not have another keyterm
          System.out.println("Thank you.");
-         System.exit(0);
+         System.exit(0); //exits entire program
       }
    }
 }
